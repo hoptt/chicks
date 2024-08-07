0d4e656630079c98f735371ebe2f536953a6d8ab
hoptt <kmertvv531@gmail.com>
2024-08-07 16:35:25 +0900

    project

--- src/components/content/security/CFsecret.tsx
+++ src/components/content/security/CFsecret.tsx
@@ -9,6 +9,7 @@ export default function CFsecret({ handleCf }: Props) {
       window.turnstile.render("#cfsc", {
         sitekey: `${import.meta.env.VITE_CF_SECRET_KEY}`,
         callback: async (token: any) => {
+          console.log(token);
           const response = await fetch(
             `${import.meta.env.VITE_BACKEND_URL}/check-cf`,
             {

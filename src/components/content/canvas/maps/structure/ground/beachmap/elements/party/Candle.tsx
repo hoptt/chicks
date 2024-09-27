db57744f5846fbc257142b9cf8eead3615f5821d
hoptt <kmertvv531@gmail.com>
2024-09-27 22:00:18 +0900

    project

--- src/components/content/canvas/maps/structure/ground/beachmap/elements/party/Candle.tsx
+++ src/components/content/canvas/maps/structure/ground/beachmap/elements/party/Candle.tsx
@@ -3,18 +3,18 @@ https://poly.pizza/m/aH83BlSFxJu
 Candle by Poly by Google [CC-BY] via Poly Pizza
 */
 
-import { useGLTF } from "@react-three/drei";
-// const fontUrl = "/fonts/Pretendard.json";
-// const fontStyle = {
-//   font: fontUrl,
-//   size: 0.08,
-//   letterSpacing: 0.01,
-//   height: 0.01,
-//   bevelEnabled: true,
-//   bevelOffset: 0.003,
-//   bevelSize: 0.001,
-//   bevelThickness: 0.001,
-// };
+import { Text3D, useGLTF } from "@react-three/drei";
+const fontUrl = "/fonts/Pretendard.json";
+const fontStyle = {
+  font: fontUrl,
+  size: 0.08,
+  letterSpacing: 0.01,
+  height: 0.01,
+  bevelEnabled: true,
+  bevelOffset: 0.003,
+  bevelSize: 0.001,
+  bevelThickness: 0.001,
+};
 export function Candle() {
   const { nodes, materials }: { nodes: any; materials: any } =
     useGLTF("/models/Candle.glb");
@@ -24,14 +24,14 @@ export function Candle() {
         geometry={nodes.Cylinder001_Cylinder002.geometry}
         material={materials.phong1SG}
       />
-      {/* <Text3D
+      <Text3D
         {...(fontStyle as any)}
         rotation={[0, 0.7, 0]}
         position={[-0.05, 0.12, 0.05]}
       >
         24
         <meshBasicMaterial color={"#98ae47"} />
-      </Text3D> */}
+      </Text3D>
     </group>
   );
 }
